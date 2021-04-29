609c2e340b904081d6d14a10b3abf7a8c583bd5f
lu-blue <lubov.novozhilova@gmail.com>
2021-04-29 09:19:42 +0200

    Killed off Update method and extra Event fields in BE for now

--- src/main/java/hikingBuddy/events/EventService.java
+++ src/main/java/hikingBuddy/events/EventService.java
@@ -14,7 +14,7 @@ public class EventService {
 
     public Event saveEvent (Event event){ return eventRepository.save(event); }
 
-    public Event updateEvent(Long id, Event eventParam, Event existingEvent){
+    /*// public Event updateEvent(Long id, Event eventParam, Event existingEvent){
         existingEvent.setEventTitle(eventParam.getEventTitle());
         existingEvent.setEventOrg(eventParam.getEventOrg());
         existingEvent.setEventTrail(eventParam.getEventTrail());
@@ -26,7 +26,7 @@ public class EventService {
         existingEvent.setEventMeetPoint(eventParam.getEventMeetPoint());
         Event event = eventRepository.save(existingEvent);
         return event;
-    }
+    }*/
 
 
 }
