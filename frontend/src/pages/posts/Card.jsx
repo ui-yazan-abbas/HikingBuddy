f4fef523294f131b9bc3a4273afd3c4b0e6d00ec
MartaLourido <martalouri@gmail.com>
2021-05-05 10:55:25 +0200

    created media buttons for sharing in external social media the posts in the post page

--- frontend/src/pages/posts/Card.jsx
+++ frontend/src/pages/posts/Card.jsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
-
 import CommentCard from "../comments/CommentCard";
 import CommentForm from "../comments/CommentForm";
 import CommentsApi from "../../api/CommentsApi";
 import PostsApi from "../../api/PostsApi";
 import UpdateCard from "./UpdateCard";
-import moment from "moment"; 
-import { Button, Comment, Form, Header } from "semantic-ui-react";
-import 'semantic-ui-css/semantic.min.css'
+import moment from "moment";
+import { Button, Comment, Form, Header, Icon } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+//  Importing the buttons to be used for react share
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share";
 
 
-export default function PostCard({ post, onDeleteClick, onUpdateClick }) {
+export default function PostCard({
+  post,
+  onDeleteClick,
+}) {
   const [isUpdating, setIsUpdating] = useState(false);
   const [comments, setComments] = useState([]);
   const [postTitle, setPostTitle] = useState(post.body);
@@ -56,69 +67,97 @@ export default function PostCard({ post, onDeleteClick, onUpdateClick }) {
   }, [setComments]);
 
   // Components
-  
-let filteredCommentList = comments.filter(item => item.commentedPost == post.id)
+
+  let filteredCommentList = comments.filter(
+    (item) => item.commentedPost == post.id
+  );
+
   return (
- <div className="postcard">
-    <Comment.Group>
-      <Comment>
-        <Comment.Avatar
-          as="a"
-          src="https://react.semantic-ui.com/images/avatar/small/steve.jpg"
-        />
-        <Comment.Content>
-        <p></p>
+    <div className="postcard">
+      <Comment.Group>
+        <Comment>
+          <Comment.Avatar
+            as="a"
+            src="https://react.semantic-ui.com/images/avatar/small/steve.jpg"
+          />
+          <Comment.Content>
+            <p></p>
 
-        <div className="container">
-          <Comment.Author as="a"> {post.user}</Comment.Author>
-          <Comment.Metadata>
-            <div>{moment(post.createAt).format("DD/MM/YYYY hh:mm:ss A")}</div>
-          </Comment.Metadata>
-          <Comment.Text>{postTitle}</Comment.Text>
-         
-          <Comment.Actions>
-            <Comment.Action active>Reply</Comment.Action>
-            
-            <Comment.Action active onClick={() => setIsUpdating(true)}>
-              Edit Post
-            </Comment.Action>
-            <Comment.Action onClick={onDeleteClick} active>
-              {" "}
-              Delete post
-            </Comment.Action>
-           
-          </Comment.Actions>
-          </div>
+            <div className="container">
+              <Comment.Author as="a"> {post.user}</Comment.Author>
+              <Comment.Metadata>
+                <div>
+                  {moment(post.createAt).format("DD/MM/YYYY hh:mm:ss A")}
+                </div>
+              </Comment.Metadata>
+              <Comment.Text>{postTitle}</Comment.Text>
 
-          
+              <Comment.Actions>
+                <Comment.Action active>Reply</Comment.Action>
 
-        
-          <div className="comments-container">
-          {comments
-            && filteredCommentList.map((comment) => (
-                <CommentCard
-                  key={post.id}
-                  comment={comment}
-                  onDeleteClick={() => deleteComment(comment)}
-                />
-              ))
-            }
-        </div>
-        
-          {isUpdating && (
-            <UpdateCard
-              onUpdateClick={(postData) => updatePost(postData)}
-              post={post}
-              onSubmite={() => setIsUpdating(false)}
-            />
-          )}
+                <Comment.Action active onClick={() => setIsUpdating(true)}>
+                  Edit Post
+                </Comment.Action>
+                <Comment.Action onClick={onDeleteClick} active>
+                  {" "}
+                  Delete post
+                </Comment.Action>
 
-          <div className="comments-form">
-            <CommentForm id={post.id} onSubmit={createComment} />
-          </div>
-        </Comment.Content>
-      </Comment>
-    </Comment.Group>
- </div>
+                {/* Buttons for share to social media  */}
+
+                <FacebookShareButton
+                  url={window.location.href}//share the actual link of the post
+                  title={post.user} //the user who wrote the post 
+                  description={postTitle}//the comment written in the post is shared
+                  quote="link"
+                >
+                  <FacebookIcon className="mx-3" size={36} round />
+                </FacebookShareButton>
+                <TwitterShareButton
+                  url={window.location.href}
+                  title={postTitle}//the comment written in the post is shared
+                  quote="link"
+                  hashtag="hiking"
+                >
+                  <TwitterIcon className="mx-3" size={36} round />
+                </TwitterShareButton>
+                <WhatsappShareButton
+                  url={window.location.href}
+                  separator = ""
+                  title={postTitle}//the comment written in the post is shared
+                  quote="link"
+                >
+                  <WhatsappIcon size={40} round={true} />
+                </WhatsappShareButton>
+                {/* Buttons for share to social media finish here  */}
+              </Comment.Actions>
+            </div>
+
+            <div className="comments-container">
+              {comments &&
+                filteredCommentList.map((comment) => (
+                  <CommentCard
+                    key={post.id}
+                    comment={comment}
+                    onDeleteClick={() => deleteComment(comment)}
+                  />
+                ))}
+            </div>
+
+            {isUpdating && (
+              <UpdateCard
+                onUpdateClick={(postData) => updatePost(postData)}
+                post={post}
+                onSubmite={() => setIsUpdating(false)}
+              />
+            )}
+
+            <div className="comments-form">
+              <CommentForm id={post.id} onSubmit={createComment} />
+            </div>
+          </Comment.Content>
+        </Comment>
+      </Comment.Group>
+    </div>
   );
 }
