591dd9e528d4158e8d51cdfc35ff3e04e5c3555d
lu-blue <lubov.novozhilova@gmail.com>
2021-05-05 22:04:19 +0200

    Added all new fields and text boxes in EventsForm jsx

--- frontend/src/pages/events/EventsForm.jsx
+++ frontend/src/pages/events/EventsForm.jsx
@@ -1,35 +1,130 @@
 import React from "react";
 
+import React, { Component } from "react";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  Radio,
+  Select,
+  TextArea,
+} from "semantic-ui-react";
+
 export default function EventsForm({ onSubmit }) {
+  const [trailName, setTrailName] = React.useState("");
+  const [eventDuration, setEventDuration] = React.useState("");
+  const [eventDistance, setEventDistance] = React.useState("");
+  const [eventDifficulty, setEventDifficulty] = React.useState("");
+  const [maxNum, setMaxNum] = React.useState("");
+  const [meetPoint, setMeetPoint] = React.useState("");
+  const [trailHyperlink, setTrailHyperlink] = React.useState("");
   const [body, setBody] = React.useState("");
 
   const handleSubmit = () => {
     // Invoke the passed in event callback
-    onSubmit({ body: body });
+    onSubmit({
+      trailName: trailName,
+      eventDuration: eventDuration,
+      eventDistance: eventDistance,
+      eventDifficulty: eventDifficulty,
+      maxNum: maxNum,
+      meetPoint: meetPoint,
+      trailHyperlink: trailHyperlink,
+      body: body,
+    });
 
     // Clear the input field
+    setTrailName("");
+    setEventDuration("");
+    setEventDistance("");
+    setEventDifficulty("");
+    setMaxNum("");
+    setMeetPoint("");
+    setTrailHyperlink("");
     setBody("");
   };
 
   return (
     <div className="card">
       <div className="card-body">
-        <h4 className="card-title">Create a new event</h4>
+        <h4 className="card-title">Create a hiking event</h4>
         <div>
-          <div className="form-group">
-            <textarea
-              className="form-control"
-              placeholder="Where should we hike this time?"
+          <Form>
+            <Form.Group widths="equal">
+              <Form.Field
+                control={Input}
+                label="Location/Trail name"
+                placeholder="Höga Kusten"
+                value={trailName}
+                onChange={(e) => setTrailName(e.target.value)}
+              />
+
+              <Form.Field
+                control={Input}
+                label="Duration in days"
+                placeholder="3"
+                width={2}
+                value={eventDuration}
+                onChange={(e) => setEventDuration(e.target.value)}
+              />
+
+              <Form.Field
+                control={Input}
+                label="Distance in km"
+                placeholder="83"
+                width={2}
+                value={eventDistance}
+                onChange={(e) => setEventDistance(e.target.value)}
+              />
+
+              <Form.Field
+                control={Input}
+                label="Max participants"
+                placeholder="7"
+                width={2}
+                value={maxNum}
+                onChange={(e) => setMaxNum(e.target.value)}
+              />
+
+              <Form.Field
+                control={Input}
+                label="Difficulty"
+                placeholder="Easy/Medium/Expert"
+                value={eventDifficulty}
+                onChange={(e) => setEventDifficulty(e.target.value)}
+              />
+
+              <Form.Field
+                control={Input}
+                label="Meeting point"
+                placeholder="Central station, Stockholm"
+                value={meetPoint}
+                onChange={(e) => setMeetPoint(e.target.value)}
+              />
+
+              <div class="ui labeled input">
+                <label>Website link to location/link</label>
+                <div class="ui label label">http://</div>
+                <input type="text" placeholder="www.hogakusten.com/en" />
+                value={trailHyperlink}
+                onChange={(e) => setTrailHyperlink(e.target.value)}
+              </div>
+            </Form.Group>
+            <Form.Field
+              control={TextArea}
+              label="About event"
+              placeholder="It´s an easy trail I took before and now..."
               value={body}
               onChange={(e) => setBody(e.target.value)}
             />
-          </div>
 
-          <div className="form-group">
-            <button className="btn btn-info" onClick={handleSubmit}>
+            <Form.Field control={Button}>
+              {" "}
+              onClick={handleSubmit}
               Create event
-            </button>
-          </div>
+            </Form.Field>
+          </Form>
         </div>
       </div>
     </div>
