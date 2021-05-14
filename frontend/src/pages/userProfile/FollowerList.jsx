3b45f04bae1770e39f464ba410777d12c49a4590
MartaLourido <martalouri@gmail.com>
2021-05-14 03:22:27 +0200

    changed followers card in user profile and added date ago to check when a user started to follow someone

--- frontend/src/pages/userProfile/FollowerList.jsx
+++ frontend/src/pages/userProfile/FollowerList.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import UserApi from "../../api/UserApi";
+import { Card, Feed } from 'semantic-ui-react'
+import moment from "moment";
+
 
 export default function FollowerList({ match }) {
   const [user, setUser] = useState({});
@@ -14,23 +17,50 @@ export default function FollowerList({ match }) {
   }, [info]);
 
   return (
-    <div>
-      <h3>{user.name}'s Followers:</h3>
-      {user.followersList?.map((i) => (
-        <>
-          <Link to={`/${i.name}/profile`}>
-            {" "}
-            <img
-              className="list-img"
-              src={i.imageUrl || "https://www.linkpicture.com/q/2_20.jpeg"}
-              alt="follwer-profile"
-            />{" "}
-          </Link>
-          <Link to={`/${i.name}/profile`}>
-            <li className="list-name"id={i.id}>{i.name}</li>
-          </Link>
-        </>
-      ))}
-    </div>
+    <Card centered>
+    <Card.Content>
+      <Card.Header basic color="green">{user.name}'s Followers:</Card.Header>
+    </Card.Content>
+    <Card.Content>
+      <Feed>
+      
+        <Feed.Event>
+          <Feed.Label image='/images/avatar/small/molly.png' />
+          <Feed.Content>
+            <Feed.Date content= {moment('Thu May 13 2021 17:30:03 GMT+0300').fromNow(true)}  /> 
+            <Feed.Summary>
+               <a>Molly Malone</a> started to follow you.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+
+      
+      </Feed>
+    </Card.Content>
+    
+  </Card>
   );
 }
+  
+
+
+//     <div>
+//       <h3>{user.name}'s Followers:</h3>
+//       {user.followersList?.map((i) => (
+//         <>
+//           <Link to={`/${i.name}/profile`}>
+//             {" "}
+//             <img
+//               className="list-img"
+//               src={i.imageUrl || "https://www.linkpicture.com/q/2_20.jpeg"}
+//               alt="follwer-profile"
+//             />{" "}
+//           </Link>
+//           <Link to={`/${i.name}/profile`}>
+//             <li className="list-name"id={i.id}>{i.name}</li>
+//           </Link>
+//         </>
+//       ))}
+//     </div>
+//   );
+// }
