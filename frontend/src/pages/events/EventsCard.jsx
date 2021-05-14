927781d94c2c9e78ec9a67a8e1bc257d88cf7b17
codeisma <ism-celik@hotmail.com>
2021-05-14 15:30:05 +0200

    changed map and clickable location name

--- frontend/src/pages/events/EventsCard.jsx
+++ frontend/src/pages/events/EventsCard.jsx
@@ -3,7 +3,7 @@ import JoinButton from "./JoinButton";
 import EventsApi from "../../api/EventsApi";
 import UpdateEvent from "./UpdateEvent";
 import moment from "moment";
-import Linkify from "react-linkify";
+import map from "../../assets/map.png"
 
 import EventCommentsApi from "../../api/EventCommentsApi";
 import EventCommentCard from "../eventComments/EventCommentCard";
@@ -216,25 +216,23 @@ export default function EventsCard({ event, onDeleteClick, user }) {
                 </Comment.Metadata>
          
 
-            <br></br>
+          <br></br>
             {/* <br></br>
               <br></br> */}
             <Segment.Group>
               <Segment textAlign="center">
                 <Comment.Text>
                   {" "}
-                  <h4>
-                    <b>Trail Location:</b> {isNewTrailName}
+                  <a href={isNewHyperlink}>Trail Location: {isNewTrailName} 
                     <br></br>
-                    {/* <b>Check out:</b> <Linkify>{isNewHyperlink}</Linkify> */}
-                  </h4>
+                  </a>
                 </Comment.Text>
 
                 <Comment.Metadata>
                   <Image
                     href={isNewHyperlink}
                     target="_blank"
-                    src="https://stfturist-en.imgix.net/app/uploads/sites/2/2017/05/stf-vandringsleder-hogakustenleden.jpg?auto=format%2Cenhance"
+                    src={map}
                   />
                 </Comment.Metadata>
 
