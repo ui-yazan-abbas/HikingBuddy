35acf2c5347519f0c6fdf7e4a679f35b0ce41acd
MartaLourido <martalouri@gmail.com>
2021-05-16 17:16:19 +0200

    deleted join component because it's no necessary anymore

--- frontend/src/pages/events/EventsCard.jsx
+++ frontend/src/pages/events/EventsCard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import JoinButton from "./JoinButton";
 import EventsApi from "../../api/EventsApi";
 import UpdateEvent from "./UpdateEvent";
 import moment from "moment";
