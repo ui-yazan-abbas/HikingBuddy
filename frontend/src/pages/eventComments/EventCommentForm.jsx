dcec2eb49ff940666645bcc933436ee8ab3eadae
codeisma <ism-celik@hotmail.com>
2021-05-17 17:40:16 +0200

    save

--- frontend/src/pages/eventComments/EventCommentForm.jsx
+++ frontend/src/pages/eventComments/EventCommentForm.jsx
@@ -12,6 +12,7 @@ export default function EventCommentForm({ id, onSubmit }) {
     const newValue = removeTags(value);
     console.log(newValue);
 
+    
     onSubmit({ body: newValue });
 
     // Clear the input field
