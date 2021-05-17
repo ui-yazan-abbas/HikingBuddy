13abf9fc684e6dd2d082bd2f002e6d420c16198c
Yazan <yazann.abbas@gmail.com>
2021-05-17 12:20:23 +0200

    heroku deploy

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Auth from "../services/Auth";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://radiant-hamlet-12155.herokuapp.com";
 
 const Api = axios.create({
   baseURL: BASE_URL,
