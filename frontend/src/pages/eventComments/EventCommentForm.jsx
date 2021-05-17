f88192b539b921a80098f753af11614fa35fd0bc
codeisma <ism-celik@hotmail.com>
2021-05-17 17:17:17 +0200

    saave

--- frontend/src/pages/eventComments/EventCommentForm.jsx
+++ frontend/src/pages/eventComments/EventCommentForm.jsx
@@ -14,7 +14,7 @@ export default function EventCommentForm({ id, onSubmit }) {
     const newValue = removeTags(value);
     console.log(newValue);
 
-    onSubmit({ "eduardo!!!!" });
+    onSubmit("eduardo");
 
     // Clear the input field
     setValue("");
